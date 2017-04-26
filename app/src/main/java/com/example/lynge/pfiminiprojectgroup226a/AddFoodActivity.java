6f1d12b34ba43e10d0ea2e0c86b94d637a8b8132
LynzZen <mathias.s.lynge@gmail.com>
2017-04-26 08:35:13 +0200

    Added arrays to values/string for the spinners on screen 5 Made the spinners on screen 5 work

--- app/src/main/java/com/example/lynge/pfiminiprojectgroup226a/AddFoodActivity.java
+++ app/src/main/java/com/example/lynge/pfiminiprojectgroup226a/AddFoodActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddFoodActivity extends AppCompatActivity {
 
@@ -11,9 +13,25 @@ public class AddFoodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen5);
+        SpinnerFoodType();
+        SpinnerFoodSize();
     }
     public void skip(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+    //Adds items to the type spinner from values/string
+    public void SpinnerFoodType(){
+        Spinner spinnerDrinkType = (Spinner) findViewById(R.id.FoodTypeSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.food_type_array,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDrinkType.setAdapter(adapter);
+    }
+    //Adds items to the size spinner from values/string
+    public void SpinnerFoodSize(){
+        Spinner spinnerDrinkType = (Spinner) findViewById(R.id.FoodSizeSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.food_size_array,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDrinkType.setAdapter(adapter);
     }
 }
