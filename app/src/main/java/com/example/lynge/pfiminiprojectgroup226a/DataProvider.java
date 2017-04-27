e8ccdced4e25c98284cc160f4f6d92eee69945fe
LynzZen <mathias.s.lynge@gmail.com>
2017-04-27 23:28:19 +0200

    Final edition

--- app/src/main/java/com/example/lynge/pfiminiprojectgroup226a/DataProvider.java
+++ app/src/main/java/com/example/lynge/pfiminiprojectgroup226a/DataProvider.java
@@ -10,29 +10,16 @@ public class DataProvider {
     private String Food;
     private String TableNumber;
 
-
     public String getDrink() {
         return Drink;
-    }
-
-    public void setDrink(String drink) {
-        Drink = drink;
     }
 
     public String getFood() {
         return Food;
     }
 
-    public void setFood(String food) {
-        Food = food;
-    }
-
     public String getTableNumber() {
         return TableNumber;
-    }
-
-    public void setTableNumber(String tableNumber) {
-        TableNumber = tableNumber;
     }
 
     public DataProvider(String Drink, String Food, String TableNumber){
